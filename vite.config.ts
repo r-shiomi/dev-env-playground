4d33aa2e6b163fb79f1f444fdafd42eaba217438
r-shiomi <sryb.0517@gmail.com>
2024-08-01 07:00:33 +0900

    ビルドオプション追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // distフォルダに出力
+    outDir: resolve(__dirname, 'dist'),
+    // 存在しないときはフォルダを作成する
+    emptyOutDir: true,
+    rollupOptions: {
+      // entry pointがあるindex.htmlのパス
+      input: {
+        '': resolve(__dirname, 'index.html'),
+      },
+      // bundle.jsを差し替えする
+      output: {
+        entryFileNames: `assets/[name]/bundle.js`,
+      },
+    },
+  }
 })
