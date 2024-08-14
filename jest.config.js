4f758ad1714593122dce0faeddf912daa3105796
r-shiomi <sryb.0517@gmail.com>
2024-08-14 18:33:24 +0900

    テスト環境をjest-environment-jsdomに変更

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
-  testEnvironment: 'node',
+  testEnvironment: 'jest-environment-jsdom',
   transform: {
     '^.+.tsx?$': ['ts-jest', {}],
   },
