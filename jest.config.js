caa0ff395d01dc1fac6b53fc5d934d4331a6b61c
r-shiomi <sryb.0517@gmail.com>
2024-08-17 17:20:35 +0900

    Revert "jest.config.js transformをbabel-jestに変更"
    This reverts commit edbf1c40d3a1d3b5c401a54eac011dcbcc8e0b33.

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 export default {
   testEnvironment: 'jest-environment-jsdom',
   transform: {
-    '\\.[jt]sx?$': 'babel-jest',
+    '^.+.tsx?$': ['ts-jest', {}],
   },
   setupFilesAfterEnv: ['./jest.setup.ts'],
 }
