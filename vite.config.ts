81f3ad8bf74d039fe48eac6f769feafae8bed1c1
r-shiomi <sryb.0517@gmail.com>
2024-08-17 17:35:30 +0900

    パスエイリアスを反映できるように

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'path'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   build: {
     // distフォルダに出力
     outDir: resolve(__dirname, 'dist'),
